55b714fa1c332edccb8dd56a69f838ff23a47d9c
Linarz11 <linarz@bk.ru>
2021-05-19 11:28:58 +0300

    Add project files.

---
+++ Passenger_Plane.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Passenger_Plane:Plane
+
+    {
+        public int Seats_count { get; set; };
+
+    }
+}
---
+++ Plane.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Plane
+    {
+        public int engine_count;
+        public int pilot_seats_count;
+
+    }
+}
